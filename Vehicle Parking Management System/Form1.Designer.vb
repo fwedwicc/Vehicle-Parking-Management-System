f5e51f7afe5f44710e471054091fbfe3187709fc
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-03-22 18:29:26 +0800

    Add project files.

---
+++ Vehicle Parking Management System/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Guna2Elipse1 = New Guna.UI2.WinForms.Guna2Elipse(Me.components)
+        Me.Username = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.Password = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.LoginBtn = New Guna.UI2.WinForms.Guna2Button()
+        Me.ProgressBar = New Guna.UI2.WinForms.Guna2ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Clear = New Guna.UI2.WinForms.Guna2Button()
+        Me.SuspendLayout()
+        '
+        'Guna2Elipse1
+        '
+        Me.Guna2Elipse1.TargetControl = Me
+        '
+        'Username
+        '
+        Me.Username.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.Username.DefaultText = ""
+        Me.Username.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.Username.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.Username.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.Username.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.Username.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Username.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Username.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Username.Location = New System.Drawing.Point(410, 165)
+        Me.Username.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Username.Name = "Username"
+        Me.Username.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.Username.PlaceholderText = ""
+        Me.Username.SelectedText = ""
+        Me.Username.Size = New System.Drawing.Size(229, 48)
+        Me.Username.TabIndex = 0
+        '
+        'Password
+        '
+        Me.Password.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.Password.DefaultText = ""
+        Me.Password.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.Password.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.Password.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.Password.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.Password.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Password.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Password.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Password.Location = New System.Drawing.Point(410, 273)
+        Me.Password.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Password.Name = "Password"
+        Me.Password.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.Password.PlaceholderText = ""
+        Me.Password.SelectedText = ""
+        Me.Password.Size = New System.Drawing.Size(229, 48)
+        Me.Password.TabIndex = 1
+        '
+        'LoginBtn
+        '
+        Me.LoginBtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.LoginBtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.LoginBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.LoginBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.LoginBtn.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.LoginBtn.ForeColor = System.Drawing.Color.White
+        Me.LoginBtn.Location = New System.Drawing.Point(392, 415)
+        Me.LoginBtn.Name = "LoginBtn"
+        Me.LoginBtn.Size = New System.Drawing.Size(229, 45)
+        Me.LoginBtn.TabIndex = 2
+        Me.LoginBtn.Text = "LoginBtn"
+        '
+        'ProgressBar
+        '
+        Me.ProgressBar.Location = New System.Drawing.Point(29, 507)
+        Me.ProgressBar.Name = "ProgressBar"
+        Me.ProgressBar.Size = New System.Drawing.Size(824, 30)
+        Me.ProgressBar.TabIndex = 3
+        Me.ProgressBar.Text = "Guna2ProgressBar1"
+        Me.ProgressBar.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 15
+        '
+        'Clear
+        '
+        Me.Clear.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.Clear.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.Clear.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.Clear.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.Clear.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Clear.ForeColor = System.Drawing.Color.White
+        Me.Clear.Location = New System.Drawing.Point(627, 415)
+        Me.Clear.Name = "Clear"
+        Me.Clear.Size = New System.Drawing.Size(229, 45)
+        Me.Clear.TabIndex = 4
+        Me.Clear.Text = "Guna2Button1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(900, 582)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Clear)
+        Me.Controls.Add(Me.ProgressBar)
+        Me.Controls.Add(Me.LoginBtn)
+        Me.Controls.Add(Me.Password)
+        Me.Controls.Add(Me.Username)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Guna2Elipse1 As Guna.UI2.WinForms.Guna2Elipse
+    Friend WithEvents Username As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents ProgressBar As Guna.UI2.WinForms.Guna2ProgressBar
+    Friend WithEvents LoginBtn As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Password As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Clear As Guna.UI2.WinForms.Guna2Button
+End Class
---
+++ Vehicle Parking Management System/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub LoginBtn_Click(sender As Object, e As EventArgs) Handles LoginBtn.Click
+        If Username.Text = "ADMIN" And Password.Text = "admin" Then
+            Timer1.Start()
+        Else
+            MsgBox("Your Input is Invalid! Try again.")
+        End If
+    End Sub
+    Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click
+        Username.Text = ""
+        Password.Text = ""
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar.Value += 1
+        If ProgressBar.Value = ProgressBar.Maximum Then
+            Timer1.Enabled = False
+            Hide()
+            MsgBox("Success! Welcome to Vehicle Parking Management System!")
+            ' Form2.Show()
+        End If
+    End Sub
+End Class
